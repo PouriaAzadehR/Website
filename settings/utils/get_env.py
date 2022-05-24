43879af1b1bacf4dff38f91ce8a155f67dd411ba
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-05-24 13:59:21 -0700

    get env

---
+++ settings/utils/get_env.py
@@ -0,0 +1,13 @@
+from os import environ
+
+
+# GET ENV UTIL
+def get_env(key, default=None, optinal=False):
+    """Return environment variables with some options."""
+    val = environ.get(key)
+    if val is not None:
+        return val
+    elif default is not None:
+        return default
+    elif not optinal:
+        raise ValueError(f"Environment variable {key} was not defined")
