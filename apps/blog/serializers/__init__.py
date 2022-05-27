e7b820233d96e37ee799cd38ea64373a08c6533b
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-05-27 01:27:57 -0700

    Create __init__.py

---
+++ apps/blog/serializers/__init__.py
@@ -0,0 +1,4 @@
+from src.apps.blog.serializers.full_news_serializer import FullNewsSerializer
+from src.apps.blog.serializers.news import NewsSerializer
+from src.apps.blog.serializers.news_category import NewsCategorySerializer
+from src.apps.blog.serializers.important_news import ImportantNewsSerializer
