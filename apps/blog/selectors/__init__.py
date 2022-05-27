4697253184a09d6137699156139073dd8cbf918c
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-05-27 01:47:19 -0700

    Create __init__.py

---
+++ apps/blog/selectors/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.blog.selectors.get_news_by_slug import get_news_by_slug
+from src.apps.blog.selectors.get_news_list import get_news_list
+from src.apps.blog.selectors.get_important_news import get_important_news_list
