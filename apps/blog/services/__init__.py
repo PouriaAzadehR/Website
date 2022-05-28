d612c8786d83b85409cd06c8b34bd185c2146ef3
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-05-28 12:03:33 -0700

    Create __init__.py

---
+++ apps/blog/services/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.blog.services.get_news_data import get_news_data
+from src.apps.blog.services.get_news_data_by_slug import get_news_data_by_slug
+from src.apps.blog.services.get_important_news import get_important_news_list
