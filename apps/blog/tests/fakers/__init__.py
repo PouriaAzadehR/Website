c4229f39b5be617a2fe45c16bdde2d4c80509de9
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-05-30 08:35:34 -0700

    Create __init__.py

---
+++ apps/blog/tests/fakers/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.blog.tests.fakers.news import NewsFactory
+from src.apps.blog.tests.fakers.news_category import NewsCategoryFactory
+from src.apps.blog.tests.fakers.media import MediaModelFactory
