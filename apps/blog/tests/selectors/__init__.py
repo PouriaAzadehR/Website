ad4a970feb3513f4ca53976b477af2339ca046c3
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-02 01:10:26 -0700

    Create __init__.py

---
+++ apps/blog/tests/selectors/__init__.py
@@ -0,0 +1,6 @@
+from src.apps.blog.tests.selectors.get_news_by_slug import (
+    GetNewsBySlugSeletorTestCase,
+)
+from src.apps.blog.tests.selectors.get_news_list import (
+    GetNewsListSelectorTestCase,
+)
