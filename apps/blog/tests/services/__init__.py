b681df1ab907cbee06e96a5d2493f9662ea8cbba
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-03 05:42:29 -0700

    Create __init__.py

---
+++ apps/blog/tests/services/__init__.py
@@ -0,0 +1,9 @@
+from src.apps.blog.tests.services.get_news_data import (
+    GetNewsDataServiceTestCase,
+)
+from src.apps.blog.tests.services.get_news_data_by_slug import (
+    GetNewsDataBySlugServiceTestCase,
+)
+from src.apps.blog.tests.services.get_important_news import (
+    GetImportantNewsServiceTestCase,
+)
