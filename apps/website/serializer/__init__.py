c31c64bbffd62e7f16394d19aa945c7b65734039
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-05 05:18:35 -0700

    Create __init__.py

---
+++ apps/website/serializer/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.website.serializers.about_us import GetAboutUsSerializer
+from src.apps.website.serializers.phone_number import GetPhoneNumberSerializer
+from src.apps.website.serializers.ticket import TicketSerializer
