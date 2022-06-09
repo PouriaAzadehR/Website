c43135a988d534bc56bfb7e888a683503bd38e0b
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-09 00:46:58 -0700

    Create __init__.py

---
+++ apps/website/tests/__init__.py
@@ -0,0 +1,4 @@
+from src.apps.website.tests.fakers import *
+from src.apps.website.tests.models import *
+from src.apps.website.tests.selectors import *
+from src.apps.website.tests.services import *
