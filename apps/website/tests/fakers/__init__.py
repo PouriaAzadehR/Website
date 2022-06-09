4113367478c814b7d2127aec502409f9631ca205
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-09 00:47:21 -0700

    Create __init__.py

---
+++ apps/website/tests/fakers/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.website.tests.fakers.about_us import AboutUsFactory
+from src.apps.website.tests.fakers.phone_number import PhoneNumberFactory
+from src.apps.website.tests.fakers.ticket import TicketFactory
