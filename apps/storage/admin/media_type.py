2f084688e6eb4b47e29f803bf5c6548b00c09d88
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-22 22:37:17 -0700

    media_type admin model

---
+++ apps/storage/admin/media_type.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from src.apps.storage.models.media_type import MediaType
+
+
+from django.contrib import admin
+
+from src.apps.storage.models.media_type import MediaType
+
+
+@admin.register(MediaType)
+class MediaTypeAdmin(admin.ModelAdmin):
+    pass
+@admin.register(MediaType)
+class MediaTypeAdmin(admin.ModelAdmin):
+    pass
