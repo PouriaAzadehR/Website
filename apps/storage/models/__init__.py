3e4ee76612474be80c57d9824b0c8878a2dfdf37
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-22 22:35:59 -0700

    Create __init__.py

---
+++ apps/storage/models/__init__.py
@@ -0,0 +1,11 @@
+from src.apps.storage.models.media import (
+    MediaModel,
+    create_checksum,
+    create_media_type,
+)
+from src.apps.storage.models.media_type import MediaType
+from src.apps.storage.models.public_media import (
+    PublicMediaModel,
+    create_checksum_public,
+    create_media_type_public,
+)
