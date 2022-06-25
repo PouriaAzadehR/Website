a8c705c4a12fddb30ab0124f7a426de41c35753a
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-25 04:43:30 -0700

    public media app model

---
+++ apps/storage/admin/public_media.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from src.apps.storage.models import PublicMediaModel
+
+
+@admin.register(PublicMediaModel)
+class PublicMediaModelAdmin(admin.ModelAdmin):
+    pass
