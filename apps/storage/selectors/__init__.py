7d1a74b8185b806392a7bd57ee444208ce0019f0
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-06-27 14:03:07 -0700

    Create __init__.py

---
+++ apps/storage/selectors/__init__.py
@@ -0,0 +1,8 @@
+from src.apps.storage.selectors.create_media import (
+    create_media,
+    create_public_media,
+)
+from src.apps.storage.selectors.get_media import (
+    get_media_by_id,
+    get_public_media_by_id,
+)
