2d255b23278360942e8565223e28216c6ee386d8
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-07-09 00:32:22 -0700

    Create __init__.py

---
+++ apps/storage/test/fakers/__init__.py
@@ -0,0 +1,3 @@
+from src.apps.storage.tests.fakers.fake_image import fake_image
+from src.apps.storage.tests.fakers.media import MediaModelFactory
+from src.apps.storage.tests.fakers.public_media import PublicMediaModelFactory
