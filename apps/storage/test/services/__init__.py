7d0eba388649d8d6b8f14f4982f606f5da782a94
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-07-27 05:50:57 -0700

    Create __init__.py

---
+++ apps/storage/test/services/__init__.py
@@ -0,0 +1,9 @@
+from src.apps.storage.tests.services.create_media import (
+    CreateMediaServiceTestCase,
+)
+from src.apps.storage.tests.services.get_file_url import (
+    GetFileUrlServiceTestCase,
+)
+from src.apps.storage.tests.services.serialize_media import (
+    SerializeMediaServiceTestCase,
+)
