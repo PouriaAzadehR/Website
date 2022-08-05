b6f663a2f5f1d2f520de9604f8e3aa1bd4ea022c
Pouria Azadeh <pouriazadeh81@aut.ac.ir>
2022-08-05 06:42:50 -0700

    Create __init__.py

---
+++ apps/authentication/models/__init__.py
@@ -0,0 +1,4 @@
+from src.apps.authentication.models.otp import OneTimePassword
+from src.apps.authentication.models.user import User
+from src.apps.authentication.models.faculty import Faculty
+from src.apps.authentication.models.nc_verification import NCVerification
